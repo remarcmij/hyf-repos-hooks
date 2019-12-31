6f8f5fe240a956304347f12c8310271518e666a9
Jim Cramer <remarcmij@gmail.com>
2019-12-31 12:54:43 +0100

    Minor tweaks

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ The purpose of this plain vanilla JavaScript application is to demonstrate how a
 - As a consequence, the required DOM manipulation is inefficient as DOM elements are created and destroyed repeatedly.
 - For simplicity, error handling is omitted.
 
-This application is an implementation of the homework for the HYF JavaScript3 module, but now using hooks. The implementation of the hooks themselves was inspired and builds upon the work done by Shang Wang:
+This application is an implementation of the homework for the HYF JavaScript3 module, but now using hooks. The implementation of the hooks themselves (see `MyReact.js`) was inspired and builds upon the work done by Shang Wang:
 
 - [Deep dive: How do React hooks really work?](https://www.netlify.com/blog/2019/03/11/deep-dive-how-do-react-hooks-really-work/)
 - [Getting Closure on Hooks (JSConf Edition)](https://www.swyx.io/speaking/react-hooks)
--- src/MyReact.js
+++ src/MyReact.js
@@ -1,92 +1,99 @@
 'use strict';
 
-window.MyReact = (() => {
-  const hooks = [];
-  let hookIndex = 0;
-  let isRenderScheduled = false;
-  let _App = null;
-  let _container = null;
+{
+  const MyReact = (() => {
+    const hooks = [];
+    let hookIndex = 0;
+    let isRenderScheduled = false;
+    let _App = null;
+    let _container = null;
 
-  /**
-   * Execute a render cycle.
-   */
-  const render = () => {
-    // Render the main App component (which in turn is
-    // responsible for rendering its child components).
-    _App(_container);
+    /**
+     * Execute a render cycle.
+     */
+    const render = () => {
+      // Render the main App component (which in turn is
+      // responsible for rendering its child components).
+      _App(_container);
 
-    // Reinitialize for next render cycle
-    isRenderScheduled = false;
-    hooks.length = hookIndex;
-    hookIndex = 0;
-  };
-
-  /**
-   * Register the main app component and its mount point and initiate a first render cycle/
-   * @param {Function} App constructor function for the main component
-   * @param {HTMLElement} container parent element where the component should be mounted.
-   */
-  const renderDOM = (App, container) => {
-    _App = App;
-    _container = container;
-    render();
-  };
-
-  /**
-   * Simple useEffect hook implementation.
-   * @param {Function} cb Function to be called back conditionally depending on dependencies.
-   * @param {*} depArray Dependency array (optional)
-   */
-  const useEffect = (cb, depArray) => {
-    const oldDeps = hooks[hookIndex]; // type: array | undefined
-    let hasChanged = true;
-    if (oldDeps) {
-      hasChanged = depArray.some((dep, i) => !Object.is(dep, oldDeps[i]));
-    }
-    if (hasChanged) cb();
-    hooks[hookIndex] = depArray;
-    hookIndex++; // done with this hook
-  };
-
-  /**
-   * Create a setState function with closed-in index value.
-   */
-  const setState = idx => {
-    return newState => {
-      // If the new state is equal to the current state
-      // then we're done.
-      if (Object.is(newState, hooks[idx][0])) return;
-
-      // Update the state value.
-      hooks[idx][0] = newState;
-
-      // Any state change should trigger a new render cycle.
-      if (!isRenderScheduled) {
-        // Schedule a new render cycle when the JavaScript engine
-        // has finished its current "run-to-completion".
-        setTimeout(render);
-        isRenderScheduled = true;
-      }
+      // Reinitialize for next render cycle
+      isRenderScheduled = false;
+      hooks.length = hookIndex;
+      hookIndex = 0;
     };
-  };
 
-  /**
-   * Simple useState hook implementation.
-   * @param {*} initialValue
-   */
-  const useState = initialValue => {
-    hooks[hookIndex] = hooks[hookIndex] || [initialValue, setState(hookIndex)];
-    return hooks[hookIndex++];
-  };
+    /**
+     * Register the main app component and its mount point and initiate a first render cycle/
+     * @param {Function} App constructor function for the main component
+     * @param {HTMLElement} container parent element where the component should be mounted.
+     */
+    const renderDOM = (App, container) => {
+      _App = App;
+      _container = container;
+      render();
+    };
 
-  /**
-   * Simple useRef hook implementation
-   * @param {*} initialValue
-   */
-  const useRef = initialValue => {
-    hooks[hookIndex] = hooks[hookIndex] || { current: initialValue };
-    return hooks[hookIndex++];
-  };
+    /**
+     * Simple useEffect hook implementation.
+     * @param {Function} cb Function to be called back conditionally depending on dependencies.
+     * @param {*} depArray Dependency array (optional)
+     */
+    const useEffect = (cb, depArray) => {
+      const oldDeps = hooks[hookIndex]; // type: array | undefined
+      let hasChanged = true;
+      if (oldDeps) {
+        hasChanged = depArray.some((dep, i) => !Object.is(dep, oldDeps[i]));
+      }
+      if (hasChanged) cb();
+      hooks[hookIndex] = depArray;
+      hookIndex++; // done with this hook
+    };
 
-  return { renderDOM, useState, useEffect, useRef };
-})();
+    /**
+     * Create a setState function with closed-in index value.
+     */
+    const setState = idx => {
+      return newState => {
+        // If the new state is equal to the current state
+        // then we're done.
+        if (Object.is(newState, hooks[idx][0])) return;
+
+        // Update the state value.
+        hooks[idx][0] = newState;
+
+        // Any state change should trigger a new render cycle.
+        if (!isRenderScheduled) {
+          // Schedule a new render cycle when the JavaScript engine
+          // has finished its current "run-to-completion".
+          setTimeout(render);
+          isRenderScheduled = true;
+        }
+      };
+    };
+
+    /**
+     * Simple useState hook implementation.
+     * @param {*} initialValue
+     */
+    const useState = initialValue => {
+      hooks[hookIndex] = hooks[hookIndex] || [
+        initialValue,
+        setState(hookIndex),
+      ];
+      return hooks[hookIndex++];
+    };
+
+    /**
+     * Simple useRef hook implementation
+     * @param {*} initialValue
+     */
+    const useRef = initialValue => {
+      hooks[hookIndex] = hooks[hookIndex] || { current: initialValue };
+      return hooks[hookIndex++];
+    };
+
+    return { renderDOM, useState, useEffect, useRef };
+  })();
+
+  window.MyReact = MyReact;
+}
